457c102e9333e44f6ff30eb073727116d6426bef
Kelar <kelar@replenium.com>
2020-01-17 16:14:15 -0800

    basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'LoginPage', component: LoginPageComponent },
+  { path: 'HomePage', component: HomePageComponent },
+  { path: 'ProfilePage', component: ProfilePageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [LoginPageComponent, HomePageComponent, ProfilePageComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent,
-    HomePageComponent,
-    ProfilePageComponent,
+    routingComponents,
     HeaderComponent
   ],
   imports: [
